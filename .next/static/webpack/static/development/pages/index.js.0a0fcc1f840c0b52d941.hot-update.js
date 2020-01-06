0dcc6404abf2ddf16690a115c20b1fd71567d393
Damianos Sotirakis <dansotirakis18@gmail.com>
2020-01-06 02:34:31 -0300

    fixNext paste

---
+++ .next/static/webpack/static/development/pages/index.js.0a0fcc1f840c0b52d941.hot-update.js
@@ -0,0 +1,44 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.jsx":
+/*!*************************!*\
+  !*** ./pages/index.jsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var isomorphic_fetch__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! isomorphic-fetch */ "./node_modules/isomorphic-fetch/fetch-npm-browserify.js");
+/* harmony import */ var isomorphic_fetch__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(isomorphic_fetch__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "C:\\Users\\Master\\reactAcademy\\ReactDeploy\\reactdeploy\\pages\\index.jsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+const Home = ({
+  repositories
+}) => __jsx("div", {
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 4
+  },
+  __self: undefined
+}, repositories.map());
+
+Home.getInitialProps = async () => {
+  const response = await fetch("https://api.github.com/users/dansotirakis/repos");
+  const repositories = await response.json();
+  return {
+    repositories
+  };
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.0a0fcc1f840c0b52d941.hot-update.js.map
