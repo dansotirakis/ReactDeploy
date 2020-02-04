25f4b845b335abdcaf4ee69169f33593adb4c75e
Damianos Sotirakis <dansotirakis18@gmail.com>
2020-02-03 23:56:19 -0300

    Structure layout

--- pages/github/index.jsx
+++ pages/github/index.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
-import { Grid } from '@material-ui/core';
+import { Grid, Box, Typography } from '@material-ui/core';
+import CodeIcon from '../../commons/Icons/CodeIcon';
 import 'isomorphic-fetch';
 
 const GitHub = ({ repositories }) => (
-  <div>
+  <Box align="center">
     <Link href="/index">
       <a>Home</a>
     </Link>
     <h1>GitHub</h1>
-    {repositories.map(repo => (
-      <Grid item xs={1}>
-        <a href={`${repo.html_url}`}>{repo.name}</a>
-      </Grid>
-    ))}
-  </div>
+    <Grid container spacing={2}>
+      {repositories.map(repo => (
+        <Grid item xs={2}>
+          <Box align="center">
+            <a href={`${repo.html_url}`}>
+              <CodeIcon />
+            </a>
+            <Typography>{repo.name}</Typography>
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+  </Box>
 );
 
 GitHub.getInitialProps = async () => {
