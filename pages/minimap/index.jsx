acb8e1859e0b70be78dd84037fa341e8247e0ab2
Damianos Sotirakis <dansotirakis18@gmail.com>
2020-02-03 21:47:21 -0300

    Structure code clean resource

--- pages/minimap/index.jsx
+++ pages/minimap/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import HereMap from '../components/HereMap';
+import HereMap from '../../components/HereMap';
 import 'isomorphic-fetch';
 
 const MiniMap = ({ repositories }) => (
