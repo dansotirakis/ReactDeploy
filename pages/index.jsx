655e645ed65045da565157c902f2c49e83531ea0
Damianos Sotirakis <dansotirakis18@gmail.com>
2020-02-03 21:15:18 -0300

    HereMap

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,40 +1,33 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 import Button from '@material-ui/core/Button';
+import 'isomorphic-fetch';
+import { Grid } from '@material-ui/core';
 
-import "isomorphic-fetch";
-import { Grid } from "@material-ui/core";
-
-const Home = ({ repositories }) => (
+const Home = () => (
   <div>
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Link href="/blog">
-          <Button variant="contained" color="secondary">
-            BLOG
+        <Link href="/github">
+          <Button variant="contained" style={{ color: '#FFF', background: '#AAADFF' }}>
+            GitHub
           </Button>
         </Link>
       </Grid>
-      {repositories.map(repo => (
-        <Grid item xs={3}>
-          <a href={`${repo.html_url}`}>
-            <Button variant="contained" color="primary">
-              {repo.name}
-            </Button>
-          </a>
-        </Grid>
-      ))}
+      <Grid item xs={12}>
+        <Link href="/heremap">
+          <Button variant="contained" style={{ color: '#FFF', background: '#AAAD' }}>
+            HereMap
+          </Button>
+        </Link>
+      </Grid>
     </Grid>
   </div>
 );
 
 Home.getInitialProps = async () => {
-  const response = await fetch(
-    "https://api.github.com/users/dansotirakis/repos"
-  );
+  const response = await fetch('https://api.github.com/users/dansotirakis/repos');
   const repositories = await response.json();
-
-  console.log(repositories);
   return { repositories };
 };
 
