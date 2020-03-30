bd8c82505cd1bd6a4f6deef8ca52af0e8b01eaf9
Damianos Sotirakis <dansotirakis18@gmail.com>
2020-03-29 23:55:43 -0300

    Ajusts

--- pages/github/index.jsx
+++ pages/github/index.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import Link from 'next/link';
-import { Grid, Box, Typography } from '@material-ui/core';
+import { Box, Grid, Typography } from '@material-ui/core';
 import CodeIcon from '../../commons/Icons/CodeIcon';
 import 'isomorphic-fetch';
+import Home from '../../commons/Icons/Home';
+import { useRouter } from 'next/router';
 
-const GitHub = ({ repositories }) => (
-  <Box align="center">
-    <Link href="/index">Home</Link>
-    <h1>GitHub</h1>
-    <Grid container spacing={2}>
-      {repositories.map(repo => (
-        <Grid item xs={12} sm={6} md={2}>
-          <Box align="center">
-            <a href={`${repo.html_url}`}>
-              <CodeIcon />
-            </a>
-            <Typography>{repo.name}</Typography>
-          </Box>
-        </Grid>
-      ))}
-    </Grid>
-  </Box>
-);
+const GitHub = ({ repositories }) => {
+  const router = useRouter();
+  return (
+    <Box align="center">
+      <Box onClick={() => router.push('/index')}>
+        <Home/>
+      </Box>
+      <h1>GitHub</h1>
+      <Grid container spacing={2}>
+        {repositories.map(repo => (
+          <Grid item xs={12} sm={6} md={2}>
+            <Box align="center">
+              <a href={`${repo.html_url}`}>
+                <CodeIcon/>
+              </a>
+              <Typography>{repo.name}</Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
 
 GitHub.getInitialProps = async () => {
   const response = await fetch('https://api.github.com/users/dansotirakis/repos');
