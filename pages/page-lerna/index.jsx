9b6cb4c44295e3391fc672e855010dde0f734851
Damianos Sotirakis <dansotirakis18@gmail.com>
2020-04-06 09:43:45 -0300

    FixNames

--- pages/page-lerna/index.jsx
+++ pages/page-lerna/index.jsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { Box } from '@material-ui/core';
 import Home from '../../commons/Icons/Home';
 
-const LernaComponent = ({ data }) => {
+const Lerna = ({ data }) => {
   const router = useRouter();
   return (
     <Box align="center" mt={'5%'} onClick={() => router.push('/index')}>
@@ -12,10 +12,10 @@ const LernaComponent = ({ data }) => {
   );
 };
 
-LernaComponent.getInitialProps = async () => {
+Lerna.getInitialProps = async () => {
   const response = await fetch('https://api.github.com/users/dansotirakis/repos');
   const data = await response.json();
   return { data };
 };
 
-export default LernaComponent;
+export default Lerna;
